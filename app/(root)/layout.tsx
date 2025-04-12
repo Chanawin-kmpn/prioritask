9ea9335b4a4dc73304390b7666cef8630a0ab2db
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-04-12 13:59:15 +0700

    update: implement footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ const layout = ({ children }: { children: ReactNode }) => {
 	return (
 		<main className="bg-light200_dark100 flex flex-col">
 			<Navbar />
-			<section className="min-h-screen flex-1">
+			<section className="-mt-[132px] min-h-screen flex-1 py-[132px]">
 				<div>{children}</div>
 			</section>
 			<Footer />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import UserAvatar from './UserAvatar';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="bg-light200_dark100 z-50 flex w-full items-center justify-between border-t px-16 py-4">
+			<div className="">
+				<Link href="/" className="flex w-fit items-center">
+					<Image
+						src="/images/prioritask-logo.png"
+						width={250}
+						height={100}
+						alt="Prioritask Logo"
+						priority
+					/>
+				</Link>
+			</div>
+			<div className="">
+				<p>Copyright &copy; Chanawin 2025</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;

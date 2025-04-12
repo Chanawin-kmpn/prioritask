3d4867274b41fd091436cf8d4924f01c1d8c1760
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-04-12 09:41:20 +0700

    update: update account ui

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { useAuth } from '@/context/Auth';
 import React from 'react';
+
+//? หลักๆหน้านี้ใช้ auth.currentUser ในแสดงและแก้ไขข้อมูลยกเว้นแก้ displayname และ update เวลาที่แก้ไข ต้องแก้ใน database ด้วย
 
 const ProfilePage = async ({ params }: RouteParams) => {
 	const { id } = await params;
